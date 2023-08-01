7d6708ed46429d2fb957de0acfe85589862159b0
Piers Gillingham <gillingham@sai-net.jp>
2023-08-01 19:07:31 +0900

    Material UI Navbar更新

--- src/views/layout/front/Header.tsx
+++ src/views/layout/front/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {withTranslation} from "react-i18next";
-import {AppBar, Box, Button, IconButton, Toolbar, Typography} from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
 
 /**
  * @param {string} className - 親からのクラス名
@@ -23,21 +23,39 @@ const TopBar = ({
 
     return (
         <Box>
-            <AppBar position="static">
+            <AppBar position="static" color={"primary"}>
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        News
-                    </Typography>
-                    <Button color="inherit">Login</Button>
+                    <Link to="/" style={{textDecoration: 'none', color: "white"}}>
+                        <Typography variant="h6" sx={{flexGrow: 1}}>
+                            ECCUBE デモ
+                        </Typography>
+                    </Link>
+                    <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}>
+                        <Link to="/entry" style={{textDecoration: 'none', color: "white"}}>
+                            <Button
+                                key={"登録"}
+                                sx={{my: 2, color: 'white', display: 'block'}}
+                            >
+                                {"登録"}
+                            </Button>
+                        </Link>
+                        <Link to="/mypage/login" style={{textDecoration: 'none', color: "white"}}>
+                            <Button
+                                key={"ログイン"}
+                                sx={{my: 2, color: 'white', display: 'block'}}
+                            >
+                                {"ログイン"}
+                            </Button>
+                        </Link>
+                        <Link to="/product/1" style={{textDecoration: 'none', color: "white"}}>
+                            <Button
+                                key={"購入"}
+                                sx={{my: 2, color: 'white', display: 'block'}}
+                            >
+                                {"購入"}
+                            </Button>
+                        </Link>
+                    </Box>
                 </Toolbar>
             </AppBar>
         </Box>
