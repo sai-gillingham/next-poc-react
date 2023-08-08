a07a0e646427ee079e265a204db1337de956f82b
Piers Gillingham <gillingham@sai-net.jp>
2023-08-08 15:37:07 +0900

    秘密トークンを消す。

--- src/state/ducks/front/login/api.ts
+++ src/state/ducks/front/login/api.ts
@@ -7,7 +7,6 @@ export function callLogin(params) {
         {
             "grant_type":"password",
             "client_id": process.env.REACT_APP_API_CLIENT_KEY,
-            "client_secret": process.env.REACT_APP_API_CLIENT_SECRET,
             "username":params.username,
             "password":params.password,
             "scope":"read write"
