56c8bf2f7b4d1e17429c1109644e48679f62c977
Piers Gillingham <gillingham@sai-net.jp>
2023-08-17 15:57:24 +0900

    新しいshoppingのreDucksファイルの作成 🦆

---
+++ src/state/ducks/front/shopping/reducers.ts
@@ -0,0 +1,26 @@
+/***
+ * /////////////////////////////////////////
+ * REDUCERS (リデューサー)
+ * -------------------
+ *
+ * すべてのリアクトのマネージャーアカウントの一時的なデータが保存される領域。
+ * アプリケーションのどこでもデータを使用することができます。
+ *
+ * Reducerイベントは、ステートへのセッター命令のみを **実行すべき** です。
+ *
+ * このReducerのステートは、/src/state/ducks/index.jsに設定されているrequestsグループに束縛されています。
+ * //////////////////////////////////////////
+ */
+import types from "./types";
+
+export const shoppingState = {
+    
+}
+
+export default function shoppingReducer(state = shoppingState, action) {
+
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
