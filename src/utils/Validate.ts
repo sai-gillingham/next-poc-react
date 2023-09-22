39d226ee0d73f3967099897f943fef5c4d4c86f1
Piers Gillingham <gillingham@sai-net.jp>
2023-09-22 20:41:02 +0900

    要らないconsole.log()を消す。

--- src/utils/Validate.ts
+++ src/utils/Validate.ts
@@ -5,10 +5,8 @@ import {entryValidations} from "../state/ducks/front/entry";
 
 export const validator = (values, schema: Joi.ObjectSchema) => {
     const validation = schema.validate(values, {abortEarly: false});
-    console.log(validation);
     const validationObject = [];
     validation.error?.details.forEach((errors) => {
-        console.log(errors);
         validationObject[errors.context?.label] = errors.message;
     });
     return validationObject;
